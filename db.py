dcf68ee86e8d759d596fb2789843cf1010a5a2e5
Richard Poettler <richard.poettler@gmail.com>
2010-11-03 15:02:16 +0100

    initial commit

---
+++ db.py
@@ -0,0 +1,100 @@
+#!/usr/bin/env python
+
+from contextlib import contextmanager
+
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy import Table, Column, ForeignKey
+from sqlalchemy import Integer, Boolean, String
+from sqlalchemy.orm import sessionmaker, mapper, relationship
+
+metadata = MetaData()
+Session = None
+
+gathering_table = Table('gathering', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('source_type_id', Integer, ForeignKey('source_type.id')),
+        Column('source_grade', Integer),
+        Column('source_level', Integer),
+        Column('weapon_grade', Integer),
+        Column('primary_weapon', Boolean),
+        Column('job_level', Integer),
+        Column('ressource_id', Integer, ForeignKey('ressource.id')),
+        Column('quantity', Integer),
+        )
+
+source_type_table = Table('source_type', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('name', String),
+        )
+
+ressource_table = Table('ressource', metadata,
+        Column('id', Integer, primary_key=True),
+        Column('name', String),
+        )
+
+class Gathering(object):
+    def __init__(self,
+            source_type=None,
+            source_grade=1,
+            source_level=1,
+            weapon_grade=1,
+            primary_weapon=True,
+            job_level=1,
+            ressource=None,
+            quantity=0,
+            ):
+        self.source_type = source_type
+        self.source_grade = source_grade
+        self.source_level = source_level
+        self.primary_weapon = primary_weapon
+        self.job_level = job_level
+        self.ressource = ressource
+        self.quantity = quantity
+
+    def __repr__(self):
+        return u"<Gathering: type: %s grade: %s resource: %s quantity: %s>" \
+                % (
+                        self.source_type.name,
+                        self.source_grade,
+                        self.ressource.name,
+                        self.quantity,
+                        )
+
+class SourceType(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return u"<SourceType: %s>" % self.name
+
+class Ressource(object):
+    def __init__(self, name):
+        self.name = name
+
+    def __repr__(self):
+        return u"<Ressource: %s>" % self.name
+
+
+mapper(Gathering, gathering_table, properties={
+    'source_type': relationship(SourceType),
+    'ressource': relationship(Ressource),
+    })
+mapper(SourceType, source_type_table)
+mapper(Ressource, ressource_table)
+
+
+def bind(url):
+    global Session
+    engine = create_engine(url)
+    metadata.bind = engine
+    metadata.create_all()
+    Session = sessionmaker(bind=engine)
+
+@contextmanager
+def get_session():
+    session = Session()
+    try:
+        yield session
+        session.commit()
+    finally:
+        session.close()
---
+++ main.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+
+import os
+import db
+from db import get_session
+from db import Gathering, SourceType, Ressource
+
+
+def initialize_data():
+    with get_session() as session:
+        miner = SourceType('miner')
+        botanist = SourceType('botanist')
+        fisher = SourceType('fisher')
+        session.add(miner)
+        session.add(botanist)
+        session.add(fisher)
+
+        tiger_cod = Ressource('Tiger Cod')
+        malm_kelp = Ressource('Malm Kelp')
+        session.add(tiger_cod)
+        session.add(malm_kelp)
+
+        copper_ore = Ressource('Copper Ore')
+        yellow_copper_ore = Ressource('Yellow Copper Ore')
+        bone_chip = Ressource('Bone Chip')
+        session.add(copper_ore)
+        session.add(yellow_copper_ore)
+        session.add(bone_chip)
+
+        gatherings = (
+                Gathering(source_type=miner,
+                    source_grade=1,
+                    source_level=2,
+                    primary_weapon=True,
+                    job_level=11,
+                    ressource=bone_chip,
+                    quantity=3),
+                )
+        for gathering in gatherings:
+            session.add(gathering)
+
+file_name = r'c:/temp/farming.db'
+db_url = r'sqlite:///' + file_name
+
+os.remove(file_name)
+
+db.bind(db_url)
+initialize_data()
+
+with get_session() as session:
+    for gathering in session.query(Gathering).all():
+        print gathering
