cdb1bd7895e05ee74a1ea695a3fd1da165fafd83
Richard Poettler <richard.poettler@gmail.com>
2010-11-18 15:01:53 +0100

    added authentification

--- controller.py
+++ controller.py
@@ -61,11 +61,15 @@ def render_template(template=None):
 
 cherrypy.tools.render = cherrypy.Tool('before_handler', render_template)
 
+from repoze.what.predicates import not_anonymous
+
 class Controller(object):
     @cherrypy.expose()
     @cherrypy.tools.render(template='gatherings.html')
     @cherrypy.tools.close_session()
     def index(self):
+        #not_anonymous(msg='log in!')\
+            #.check_authorization(cherrypy.request.wsgi_environ)
         gatherings = Session.query(Gathering).all()
         return {'gatherings': gatherings}
 
@@ -79,6 +83,145 @@ env = Environment(loader=FileSystemLoader('templates'))
 cherrypy.config.update({
     #'tools.close_session.on': True,
     })
-cherrypy.tree.mount(Controller(), '/')
+app = cherrypy.Application(Controller())
+
+#cherrypy.tree.mount(app, '/')
+#cherrypy.engine.start()
+#cherrypy.engine.block()
+
+###############################################################################
+
+from repoze.what.middleware import setup_auth
+from repoze.who.plugins.basicauth import BasicAuthPlugin
+from repoze.what.adapters import BaseSourceAdapter
+
+class MapAuthenticator(object):
+    def __init__(self, users):
+        self.users = users
+
+    def authenticate(self, environ, identity):
+        try:
+            login = identity['login']
+            password = identity['password']
+        except KeyError:
+            return None
+        if login in self.users and self.users[login] == password:
+            return login
+        return None
+
+class DictGroupSource(BaseSourceAdapter):
+    def __init__(self, sections):
+        super(BaseSourceAdapter, self).__init__()
+        self.sections = sections
+
+    def _get_all_sections(self):
+        return self.sections
+
+    def _get_section_items(self, section):
+        return self.sections[section]
+
+    def _find_sections(self, credentials):
+        id = credentials['repoze.who.userid']
+        return set([key for key, value in self.sections.iteritems()
+                if id in value])
+
+    def _include_items(self, section, items):
+        self.sections[section] |= items
+
+    def _exclude_items(self, section, items):
+        self.sections[section] -= items
+
+    def _item_is_included(self, section, item):
+        return item in self.sections[section]
+
+    def _create_sectioin(self, section):
+        self.sections[section] = set()
+
+    def _edit_section(self, section, new_section):
+        self.sections[new_section] = self.sections[section]
+        del self.sections[section]
+
+    def _delete_section(self, section):
+        del self.sections[section]
+
+    def _section_exists(self, section):
+        return sectioin in self.sections
+
+class DictPermissionSource(BaseSourceAdapter):
+    def __init__(self, sections):
+        super(BaseSourceAdapter, self).__init__()
+        self.sections = sections
+
+    def _get_all_sections(self):
+        return self.sections
+
+    def _get_section_items(self, section):
+        return self.sections[section]
+
+    def _find_sections(self, group_name):
+        return set([key for key, value in self.sections.iteritems()
+                if group_name in value])
+
+    def _include_items(self, section, items):
+        self.sections[section] |= items
+
+    def _exclude_items(self, section, items):
+        self.sections[section] -= items
+
+    def _item_is_included(self, section, item):
+        return item in self.sections[section]
+
+    def _create_sectioin(self, section):
+        self.sections[section] = set()
+
+    def _edit_section(self, section, new_section):
+        self.sections[new_section] = self.sections[section]
+        del self.sections[section]
+
+    def _delete_section(self, section):
+        del self.sections[section]
+
+    def _section_exists(self, section):
+        return sectioin in self.sections
+
+
+user_dict = {
+    'richi': 'richi',
+    'chrissi': 'chrissi',
+    'ulli': 'ulli',
+    'steffi': 'steffi',
+}
+
+group_dict = {
+    'male': set('richi'),
+    'older': set(('richi', 'chrissi')),
+}
+
+permission_dict = {
+    'new': set(),
+    'edit': set('older'),
+    'delete': set(('male', 'older')),
+}
+
+basicauth = BasicAuthPlugin('Private section')
+identifiers = [('basicauth', basicauth)]
+
+authentificators = [('mapauth', MapAuthenticator(user_dict))]
+groups = [('all_groups', DictGroupSource(group_dict))]
+permissions = [('all_permissions', DictPermissionSource(permission_dict))]
+
+challengers = [('basicauth', basicauth)]
+
+auth_app = setup_auth(app)
+        groups,
+        permissions,
+        identifiers=identifiers,
+        authenticators=authentificators,
+        challengers=challengers,
+        )
+###############################################################################
+
+app.wsgiapp.pipeline.append(('repoze.what', auth_app))
+cherrypy.tree.mount(auth_app, '/')
 cherrypy.engine.start()
 cherrypy.engine.block()
